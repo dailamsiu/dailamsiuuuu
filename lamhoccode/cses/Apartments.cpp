5711c76be223b18584cacf7d77a9628645fac29f
dailamsiu <dailamsiu@gmail.com>
2026-07-16 02:06:17 +0000

    Jul 16, 2026, 9:06 AM

--- lamhoccode/cses/Apartments.cpp
+++ lamhoccode/cses/Apartments.cpp
@@ -23,7 +23,7 @@ void solve() {
     cin>>n>>m>>k;
     for(int i=1;i<=n;i++)   cin>>a[i];
     for(int i=1;i<=m;i++)   cin>>b[i];
-    sort(a+1,a+n+1);
+    sort(a+1,a+n+1);sort(b+1,b+m+1);
     int d=0;
     int i=1,j=1;
     while(i<=n&&j<=m)
