f9d0353194137b830a81296d63d0d913774d17b4
dailamsiu <dailamsiu@gmail.com>
2026-07-17 03:53:13 +0000

    Jul 17, 2026, 10:53 AM

---
+++ lamhoccode/clueoj/ts10/2026/Lam Dong/cau4.cpp
@@ -0,0 +1,41 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("inline")
+#include<bits/stdc++.h>
+#define int long long
+#define ld long double
+#define pb push_back
+#define pii pair<int, int>
+#define fi first
+#define se second
+#define bit(x, i) ((x >> i) & 1)
+#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
+#define FORD(i, a, b) for (int i = (a); i >= (b); --i)
+#define task "test"
+#define dailamsiu main
+using namespace std;
+mt19937 rd(chrono::steady_clock::now().time_since_epoch().count());
+int rand(int l, int r) { assert(l <= r); return uniform_int_distribution<int>(l, r)(rd); }
+const int N = 1e6 + 5;
+const int mod = 1e9+7;
+int n,m,a[N],b[N];
+void solve() {
+    cin>>n>>m;
+    for(int i=1;i<=n;i++)
+    {
+        cin>>a[i];
+    }
+    sort(a+1,a+n+1);
+    for(int i=1;i<=m;i++)
+    {
+        cin>>b[i];
+        auto it=lower_bound(a+1,a+n+1,b[i]);
+        cout<<b[i]-(it-a-1)-1;
+    }
+}
+dailamsiu() {
+    if (fopen(task".inp", "r")) { freopen(task".inp", "r", stdin); freopen(task".out", "w", stdout); }
+    ios::sync_with_stdio(0); cin.tie(0);
+    int ntest = 1; //cin >> ntest;
+    while (ntest--) solve();
+}
