512da1bca7f2a40f28a9fe2ee19b3fa749c32a2d
dailamsiu <dailamsiu@gmail.com>
2026-07-20 03:24:10 +0000

    Jul 20, 2026, 10:24 AM

---
+++ lamhoccode/clueoj/THHV/lop 10/2015/hangpics.cpp
@@ -0,0 +1,36 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("inline")
+#include<bits/stdc++.h>
+#define int long long
+#define ld long double
+#define pb push_back
+#define pii pair<int, int>
+#define fi first
+#define se second
+#define bit(x, i) ((x >> i) & 1)
+#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
+#define FORD(i, a, b) for (int i = (a); i >= (b); --i)
+#define task "test"
+#define dailamsiu main
+using namespace std;
+mt19937 rd(chrono::steady_clock::now().time_since_epoch().count());
+int rand(int l, int r) { assert(l <= r); return uniform_int_distribution<int>(l, r)(rd); }
+const int N = 1e6 + 5;
+const int mod = 1e9+7;
+int a1,b1,a2,b2,a3,b3;
+
+void solve() {
+    cin>>a1>>b1>>a2>>b2>>a3>>b3;
+     if(a2>b2)   swap(a2,b2);
+    if(a3>b3)   swap(a3,b3);
+    if(a1>b1)   swap(a1,b1);
+    if(max(b2,b3)<=b1&&max(a2,a3)<=a1)  cout<<"YES";
+    else    cout<<"NO";
+}
+dailamsiu() {
+    if (fopen(task".inp", "r")) { freopen(task".inp", "r", stdin); freopen(task".out", "w", stdout); }
+    ios::sync_with_stdio(0); cin.tie(0);
+    int ntest = 1; //cin >> ntest;
+    while (ntest--) solve();
+}
