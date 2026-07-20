8ef2e235ac3ae8f197a1657733dd9162e857af0e
dailamsiu <dailamsiu@gmail.com>
2026-07-20 03:32:26 +0000

    Jul 20, 2026, 10:32 AM

--- lamhoccode/clueoj/THHV/lop 10/2015/hangpics.cpp
+++ lamhoccode/clueoj/THHV/lop 10/2015/hangpics.cpp
@@ -19,13 +19,13 @@ int rand(int l, int r) { assert(l <= r); return uniform_int_distribution<int>(l,
 const int N = 1e6 + 5;
 const int mod = 1e9+7;
 int a1,b1,a2,b2,a3,b3;
-
+bool check(int a1,int b1,int a2,int b2,int a3,int b3)
+{
+    return ((a2+a3<=a1&&max(b2,b3)<=b1)||(max(a2,a3)<=a1&&b2+b3<=b1));
+}
 void solve() {
     cin>>a1>>b1>>a2>>b2>>a3>>b3;
-     if(a2>b2)   swap(a2,b2);
-    if(a3>b3)   swap(a3,b3);
-    if(a1>b1)   swap(a1,b1);
-    if(max(b2,b3)<=b1&&max(a2,a3)<=a1)  cout<<"YES";
+    if(check(a1,b1,a2,b2,a3,b3)||check(a1,b1,b2,a2,b3,a3)||check(a1,b1,b2,a2,a3,b3)||check(a1,b1,a2,b2,b3,a3))  cout<<"YES";
     else    cout<<"NO";
 }
 dailamsiu() {
