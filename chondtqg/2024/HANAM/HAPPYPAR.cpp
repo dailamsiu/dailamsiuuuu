945d1586bc298ad77cd93e90c41f38ba84935200
dailamsiu <dailamsiu@gmail.com>
2026-08-25 06:50:47 +0700

    a

---
+++ chondtqg/2024/HANAM/HAPPYPAR.cpp
@@ -0,0 +1,47 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("inline")
+#include<bits/stdc++.h>
+#define int long long
+#define ld long double
+#define pb push_back
+#define pii pair<int, int>
+#define fi first
+#define se second
+#define bit(x, i) ((x >> i) & 1)
+#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
+#define FORD(i, a, b) for (int i = (a); i >= (b); --i)
+#define task "test"
+#define dailamsiu main
+using namespace std;
+mt19937 rd(chrono::steady_clock::now().time_since_epoch().count());
+int rand(int l, int r) { assert(l <= r); return uniform_int_distribution<int>(l, r)(rd); }
+const int N = 1e6 + 5;
+const int mod = 998244353;
+int n,m,a[N],tongglobal,tongai;
+void solve() {
+    cin>>n>>m;
+    for(int i=1;i<=n;i++)
+    {
+        cin>>a[i];
+    }
+    if(m&1)
+    {
+        tongglobal=(m%mod)*(((m+1)/2)%mod)%mod;
+    }
+    else 
+    {
+        tongglobal=((m/2)%mod)*((m+1)%mod)%mod;
+    }
+    for(int i=1;i<=n;i++)
+    {
+        tongai=(tongai+a[i]*(i)*(n-i+1))%mod;
+    }
+    cout<<tongglobal-tongai;
+}
+dailamsiu() {
+    if (fopen(task".inp", "r")) { freopen(task".inp", "r", stdin); freopen(task".out", "w", stdout); }
+    ios::sync_with_stdio(0); cin.tie(0);
+    int ntest = 1; //cin >> ntest;
+    while (ntest--) solve();
+}
